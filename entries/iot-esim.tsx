66d282983d98b5d1ec643e1b6ea00d43a9b5b5a4
Steve Shock <steve@Steves-MacBook-Pro.local>
2025-12-18 11:58:16 -0500

    Fix navigation links for GitHub Pages

--- entries/iot-esim.tsx
+++ entries/iot-esim.tsx
@@ -5,7 +5,7 @@ import { IoTAndESimPage } from '../components/IoTAndESimPage';
 import '../index.css';
 
 const handleConnect = () => {
-    window.location.href = '/contact.html';
+    window.location.href = 'contact.html';
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
